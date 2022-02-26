4271492ac84700f10682b1be41765bbf1aa13db5
muhammadsulaymannkhan <sulayman@we-over-i.com>
2022-02-26 16:33:59 +0500

    file structure

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const counter = () => {
+  return (
+    <div style={{display:"flex",width:"100vw",height:"100vh",justifyContent:"center",alignItems:"center"}}>counter</div>
+  )
+}
+
+export default counter
