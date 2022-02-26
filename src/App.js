8978c5ca1f50961dd7929bef6d65995ab290f730
muhammadsulaymannkhan <sulayman@we-over-i.com>
2022-02-26 17:52:37 +0500

    Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Button, Text, Heading } from "grommet";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./store/counterSlice";
 
-const counter = () => {
+const App = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div style={{display:"flex",width:"100vw",height:"100vh",justifyContent:"center",alignItems:"center"}}>counter</div>
-  )
-}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100vw",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#7D4CDB50",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "48px",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "white",
+          boxShadow: " rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+          borderRadius: "5x",
+        }}
+      >
+        <Heading
+          style={{
+            fontFamily: " 'Praise', cursive,'JetBrains Mono', monospace",
+          }}
+        >
+          {" "}
+          State Management using Redux
+        </Heading>
+        <div>
+          <Button
+            primary
+            size="large"
+            style={{ marginRight: "20px" }}
+            label="Derement"
+            onClick={() => dispatch(decrement())}
+          ></Button>
+          <Text>{count}</Text>
+          <Button
+            primary
+            size="large"
+            style={{ marginLeft: "20px" }}
+            label="Inrement"
+            onClick={() => dispatch(increment())}
+          ></Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default counter
+export default App;
